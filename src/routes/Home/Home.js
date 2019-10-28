d7bab840e854ff83e8a602a7d8cd693b5432378d
Harry Chung <hc9825@gmail.com>
2019-10-27 18:16:52 -0700

    demo done

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -13,13 +13,55 @@ class Home extends React.Component {
         return (
             <section className='home-page'>
                 <div className='home-block'>
-                    <div onMouseEnter={this.toggleBean}
+                    <div onMouseEnter={() => {
+                        if (!bean) {
+                            this.toggleBean()
+                        }
+                    }}
                         className={'beano-view landing-logo' + (bean ? ' logo-active' : '')}>
                         Beano
+                        <div className={'landing-icon icon-beano' + (bean ? ' icon-active' : '')}>
+                        <div id="wrap">
+                            <div class="steam" id="steam-one"></div>
+                            <div class="steam" id="steam-two"></div>
+                            <div class="steam" id="steam-three"></div>
+                            <div class="steam" id="steam-four"></div>
+                            
+                            <div id="cup">
+                                <div id="cup-body">
+                                <div id="cup-shade"></div>
+                                </div>
+                                <div id="cup-handle"></div>
+                            </div>
+                            
+                            <div id="saucer"></div>
+                            
+                            <div id="shadow"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div onMouseEnter={this.toggleBean}
+                    <div onMouseEnter={() => {
+                        if (bean) {
+                            this.toggleBean()
+                        }
+                    }}
                         className={'vino-view landing-logo' + (!bean ? ' logo-active' : '')}>
                         Vino
+                        <div className={'landing-icon icon-vino' + (!bean ? ' icon-active' : '')}>
+                            <div class="loader">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                                <div class="wineglass left">
+                                    <div class="top"></div>
+                                </div>
+                                <div class="wineglass right">
+                                    <div class="top"></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
